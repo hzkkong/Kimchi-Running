70b39d01c3b0663f8ef48750e0d01389a474e20a
hzkkong <khj0227798@gmail.com>
2025-01-10 21:06:52 +0900

    Obstacle-Part1
    빌딩 Mover, Destroy, Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Settings")]
+    public float minSpawnDelay;
+    public float maxSpawnDelay;
+
+    [Header("References")]
+    public GameObject[] gameObjects;
+    
+    void Start()
+    {
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }
+
+    void Spawn()
+    {
+        GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
+        Instantiate(randomObject, transform.position, Quaternion.identity);
+
+        // Invoke - 클래스에서 특정 메소드를 불러올 수 있도록 해줌
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }  
+}
